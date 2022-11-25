af38180e7de5a24090a1956fd82746e0d84b509f
谭顼坤 <465199853@qq.com>
2022-11-25 17:11:08 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ canvas.height = window.document.body.clientHeight;
 
 
 
-// const textShow = new TextShow(canvas, context);
-// textShow.render();
-// textShow.run();
-// textShow.onEnd = () => {
+const textShow = new TextShow(canvas, context);
+textShow.render();
+textShow.run();
+textShow.onEnd = () => {
     const starrySky = new StarrySky(canvas, context, 1100);
     starrySky.draw();
     let meteorShower = new MeteorShower(canvas, context);
     meteorShower.start();
-    // setTimeout(() => {
+    setTimeout(() => {
         const heart = new Heart(canvas, context);
         heart.render();
-    // }, 3000)
-// }
+    }, 3000)
+}
 
 // const starrySky = new StarrySky(canvas, context, 1100);
 // starrySky.draw();
